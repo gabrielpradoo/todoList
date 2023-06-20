0438a7aba2746cf1fad4be7512781170d147621b
Gabriel <gdeoliveira.1999@gmail.com>
2023-06-20 16:29:17 -0400

    Feat: Filter and Styles Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Button, Card, CardContent, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+
 import './App.css';
 
 interface TodoProps {
@@ -11,11 +13,13 @@ interface TodoProps {
 
 function App() {
   /* ------------------- CADASTRO ------------------------ */
-  const [todos, setTodos] = useState<TodoProps[]>(JSON.parse(localStorage.getItem('todos') || '')) //
+  const [todos, setTodos] = useState<TodoProps[]>(JSON.parse(localStorage.getItem('todos') || '[]'))
   const [todoTitulo, setTodoTitulo] = useState('')
   const [todoCorpo, setTodoCorpo] = useState('')
   const [todoImportancia, setTodoImportancia] = useState('')
   const [todoData, setTodoData] = useState('')
+
+  const [filtro, setFiltro] = useState('Todos')
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
@@ -26,6 +30,7 @@ function App() {
     setTodoImportancia('')
     setTodoData('')
   }
+
   useEffect(() => {
     localStorage.setItem('todos', JSON.stringify(todos))
   }, [todos])
@@ -81,34 +86,143 @@ function App() {
             </Button>
           </form>
         </div>
-        <div className="cards">
-          <Grid
-            container spacing={2}
-            direction="column"
-            justifyContent="flex-start"
-            alignItems="stretch">
-            {todos.map((todo) => (
-              <Grid key={todo.todoTitulo} item xs="auto">
-                <Card sx={{ minWidth: 275 }}>
-                  <CardContent>
-                    <Typography variant='h5' color="text.secondary" gutterBottom style={{ fontWeight: 'bold' }}>
-                      {todo.todoTitulo}
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                      {todo.todoCorpo}
-                    </Typography>
-                    <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
-                      {todo.todoImportancia}
-                    </Typography>
-                    <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
-                      {todo.todoData}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+        <div className="cards-container">
+          <div role="presentation">
+            <Breadcrumbs aria-label="breadcrumb">
+              <button onClick={() => {
+                console.log('Todos')
+                setFiltro("Todos")
+              }}>
+                Todos
+              </button>
+              <button
+                onClick={() => {
+                  console.log('Muito Importante')
+                  setFiltro("Muito Importante")
+                }}
+              >
+                Muito
+              </button>
+              <button
+                onClick={() => {
+                  console.log('importante')
+                  setFiltro("Importante")
+                }}
+              >
+                Importante
+              </button>
+              <button
+                onClick={() => {
+                  console.log('Pouco importante')
+                  setFiltro("Pouco Importante")
+                }}
+              >
+                Pouco
+              </button>
+            </Breadcrumbs>
+          </div>
+          <div className="cards">
+            <Grid
+              container spacing={2}
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="stretch">
+
+              {todos.map((todo) => {
+                if ( //Muito
+                  todo.todoImportancia === "Muito Importante" && filtro === "Muito Importante") {
+                  return (
+                    <Grid key={todo.todoTitulo} item xs="auto">
+                      <Card sx={{ minWidth: 275, maxWidth: 400 }}>
+                        <CardContent>
+                          <Typography variant='h5' color="text.secondary" gutterBottom style={{ fontWeight: 'bold' }}>
+                            {todo.todoTitulo}
+                          </Typography>
+                          <Typography variant="body1" component="div">
+                            {todo.todoCorpo}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoImportancia}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoData}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  )
+                } else if (
+                  todo.todoImportancia === "Importante" && filtro === "Importante") {
+                  return (
+                    <Grid key={todo.todoTitulo} item xs="auto">
+                      <Card sx={{ minWidth: 275, maxWidth: 400 }}>
+                        <CardContent>
+                          <Typography variant='h5' color="text.secondary" gutterBottom style={{ fontWeight: 'bold' }}>
+                            {todo.todoTitulo}
+                          </Typography>
+                          <Typography variant="body1" component="div">
+                            {todo.todoCorpo}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoImportancia}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoData}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  )
+                } else if (todo.todoImportancia === "Pouco Importante" && filtro === "Pouco Importante") {
+                  return (
+                    <Grid key={todo.todoTitulo} item xs="auto">
+                      <Card sx={{ minWidth: 275, maxWidth: 400 }}>
+                        <CardContent>
+                          <Typography variant='h5' color="text.secondary" gutterBottom style={{ fontWeight: 'bold' }}>
+                            {todo.todoTitulo}
+                          </Typography>
+                          <Typography variant="body1" component="div">
+                            {todo.todoCorpo}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoImportancia}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoData}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  )
+                } else if (filtro === "Todos") {
+                  return (
+                    <Grid key={todo.todoTitulo} item xs="auto">
+                      <Card sx={{ minWidth: 275, maxWidth: 400 }}>
+                        <CardContent>
+                          <Typography variant='h5' color="text.secondary" gutterBottom style={{ fontWeight: 'bold' }}>
+                            {todo.todoTitulo}
+                          </Typography>
+                          <Typography variant="body1" component="div">
+                            {todo.todoCorpo}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoImportancia}
+                          </Typography>
+                          <Typography variant="body1" component="div" style={{ fontWeight: 'bold' }}>
+                            {todo.todoData}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  )
+                }
+
+              })}
+            </Grid>
+          </div>
         </div>
+
+
       </div>
     </div>
   )
